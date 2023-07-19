c051ba3b0b92ceda751e78fcf342d1e6a52f68ab
Olga.Olza <olga-rgb0006@rambler.ru>
2023-07-19 12:13:15 +0300

    Произведение всех чисел от 1 до N_Вариант 2

---
+++ Task004/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход
+// число (N) и выдает произведение чисел от 1 до (N)
+
+// 4 -> 24
+// 5 -> 120
+
+// Вариант 2
+
+int GetInfo(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = GetInfo("Введите число: ");
+int faktorial = 1;
+while (number > 0)
+{
+    faktorial = faktorial * number; // faktorial = 1 * 5 
+    number--;  // number = 5-1 и повторяем теже действия до тех пор пока number не станет 0, после выводим результат 
+}
+Console.WriteLine(faktorial);
