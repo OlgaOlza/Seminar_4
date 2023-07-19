b03e3007cb37a51b7f5100931cd0de547e654560
Olga.Olza <olga-rgb0006@rambler.ru>
2023-07-19 09:37:14 +0300

    Сумма всех числе от1 до (N)

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -5,5 +5,18 @@
 // 8 -> 36
 
 
-Console.Write("Введи число от 1 до 7: ");
-int num = int.Parse(Console.ReadLine());
+int GetInfo (string text)
+{
+Console.Write(text);
+int inputNum = Convert.ToInt32 (Console.ReadLine());
+return inputNum;
+}
+
+int numberA = GetInfo("Введите число A: ");
+int sum = 0;
+for (int i = 1; i <= numberA; i++)
+{
+    sum += i;
+}
+
+Console.WriteLine($"Сумма всех чисел от 1 до {numberA} равна {sum}");
