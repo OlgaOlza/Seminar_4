133fe9b1b028f305ea472b7168f67933f251f7ea
Olga.Olza <olga-rgb0006@rambler.ru>
2023-07-19 10:46:01 +0300

    Сколько цифр в массиве

---
+++ Task002/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу. которая принмает на вход
+// число и выдает количество цифр в числе 
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// Вариант 1
+
+int GetNum (int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count ++;
+    }
+     return count;
+}
+
+int GetInfo (string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = GetInfo("Введите число: ");
+
+Console.WriteLine($"{number} состоит из {GetNum(number)} цифр ");
+
+
+// Вариант 2
+
+
+
