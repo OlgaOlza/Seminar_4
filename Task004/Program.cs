92f62b30da6c132eb464dfb98f0075343d0f5929
Olga.Olza <olga-rgb0006@rambler.ru>
2023-07-19 12:32:38 +0300

    Произведение всех чисел от 1 до N_Вариант3

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -4,19 +4,44 @@
 // 4 -> 24
 // 5 -> 120
 
-// Вариант 2
+// // Вариант 2
+
+// int GetInfo(string message)
+// {
+// Console.Write(message);
+// return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int number = GetInfo("Введите число: ");
+// int faktorial = 1;
+// while (number > 0)
+// {
+//     faktorial = faktorial * number; // faktorial = 1 * 5 
+//     number--;  // number = 5-1 и повторяем теже действия до тех пор пока number не станет 0, после выводим результат 
+// }
+// Console.WriteLine(faktorial);
+
+
+// Вариант 3 Создаем функцию сами FindFactorial (Глагол + существительное)
+
+
+int FindFactorial(int numberA)
+{
+    int faktor = 1;
+    while (numberA > 0)
+    {
+        faktor = faktor * numberA; // faktorial = 1 * 5 
+        numberA--;  // number = 5-1 и повторяем теже действия до тех пор пока number не станет 0, после выводим результат 
+    }
+    return faktor;
+}
 
 int GetInfo(string message)
 {
-Console.Write(message);
-return Convert.ToInt32(Console.ReadLine());
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
 
 int number = GetInfo("Введите число: ");
-int faktorial = 1;
-while (number > 0)
-{
-    faktorial = faktorial * number; // faktorial = 1 * 5 
-    number--;  // number = 5-1 и повторяем теже действия до тех пор пока number не станет 0, после выводим результат 
-}
+int faktorial = FindFactorial(number);  // Создаем новую функцию сами . Глагол + существительное
 Console.WriteLine(faktorial);
