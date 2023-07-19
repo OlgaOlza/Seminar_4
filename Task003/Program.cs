81b1b99acc2445e5342046f5b4ba1babde89c5af
Olga.Olza <olga-rgb0006@rambler.ru>
2023-07-19 11:15:07 +0300

    Произведение всех чисел от 1 до N

---
+++ Task003/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход
+// число (N) и выдает произведение чисел от 1 до (N)
+
+// 4 -> 24
+// 5 -> 120
+
+int GetMul(int limit)
+{
+        int mul =1;
+        for (int i = 1; i <= limit; i++)
+        {
+            mul *= i;
+        }
+        return mul;
+ }
+
+int GetInfo (string text)
+{
+Console.Write(text);
+int inputNum = Convert.ToInt32 (Console.ReadLine());
+return inputNum;
+}
+
+int numberA = GetInfo("Введите число A: ");
+
+ Console.WriteLine($"Произведение всех чисел от 1 до {numberA} равна {GetMul(numberA)}");
+
